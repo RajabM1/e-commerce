7e83ebb546bcbb370fc27e69643d862ca8cfd822
Rajab M <rajab1.masri@gmail.com>
2024-12-21 22:33:32 +0200

    Update Category Provider
    Update category provider not enabled when user not logged in.

--- frontend/src/features/categories/context/CategoryProvider.tsx
+++ frontend/src/features/categories/context/CategoryProvider.tsx
@@ -3,10 +3,13 @@ import CategoryContext from "./CategoryContext";
 import { useQuery } from "@tanstack/react-query";
 import { queryKeys } from "../../../config/query";
 import * as Services from "./services";
+import { useAuth } from "../../auth/context";
 
 const CategoryProvider = ({ children }: PropsWithChildren) => {
+    const { currentUser } = useAuth();
     const { data: categories } = useQuery({
         initialData: [],
+        enabled: !!currentUser,
         queryKey: [queryKeys.CATEGORIES],
         queryFn: () => Services.fetchCategories(),
     });
